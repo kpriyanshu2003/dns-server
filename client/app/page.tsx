795c4f03735e0b14408df208c9afe825caa02230
Kumar Priyanshu <109672617+kpriyanshu2003@users.noreply.github.com>
2024-06-20 19:50:01 +0000

    Setup NextUI in frontend

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@nextui-org/button";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  );
 }
